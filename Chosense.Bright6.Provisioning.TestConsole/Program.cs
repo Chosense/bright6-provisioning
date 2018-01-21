45968df2a02ad46a2c1616a16ce7c865c825a777
Mika Berglund <mika.berglund@chosense.com>
2018-01-21 12:39:47 +0200

    Setting up provisioning with CSOM.

---
+++ Chosense.Bright6.Provisioning.TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Utilities;
+using System;
+using System.Net;
+
+namespace Chosense.Bright6.Provisioning.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProvisioningContext(args[0]);
+            Connect(context.SiteUrl);
+        }
+
+
+        private static void Connect(Uri url)
+        {
+            using (var context = new ClientContext(url))
+            {
+                context.Load(context.Site);
+                context.ExecuteQuery();
+
+                Console.WriteLine(context.Site.RootWeb.Title);
+            }
+
+        }
+    }
+}
